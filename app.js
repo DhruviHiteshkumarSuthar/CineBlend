66ef9d9cf31a8e5f90ebffc4534e02ca4a8f462f
Dhruvi Suthar <dhruvihsuthar21@gnu.ac.in>
2024-04-18 21:52:48 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express');
+
+var app = express();
+const port = process.env.PORT || 8000
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port,function() {
+  // console.log("server starting on " + 8000);
+});
