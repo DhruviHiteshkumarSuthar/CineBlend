a31c18ee745590202e9a9e1ccfe304433a2202b0
Prarthi Joshi <prarthikjoshi21@gnu.ac.in>
2024-05-07 09:45:58 +0530

    prarthi

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-// var express = require('express');
-
-// var app = express();
-// const port = process.env.PORT || 8080
-// app.use(express.static(__dirname + '/public'));
-
-// app.listen(port,function() {
-//   // console.log("server starting on " + 8000);
-// });
 var express = require('express');
+var app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 var PORT;
 var Cloudant = require('@cloudant/cloudant');
 
@@ -20,31 +14,33 @@ if (process.env.PORT) {
 var url = "https://apikey-v2-182sso54wjwbhfsn4vutlvd52tohwxm5tr5neu0vzjf8:ef898583465dfc2b6ae676f89dbe74ca@fda58982-1434-460f-8929-dfc3615c3c07-bluemix.cloudantnosqldb.appdomain.cloud";
 var username = "apikey-v2-182sso54wjwbhfsn4vutlvd52tohwxm5tr5neu0vzjf8";
 var password = "ef898583465dfc2b6ae676f89dbe74ca";
-var app = express();
-const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
 app.post('/signup', (req, res) => {
-  const { name, email, upassword } = req.body;
+  const {name, email, upassword} = req.body;
+  const name1=req.body.name;
+  const name2=req.body['name'];
+  console.log(name1, name2);
+  console.log('Received data:', name, email, upassword);
 
-  // Initialize Cloudant
   const cloudant = Cloudant({ url: url, username: username, password: password });
 
   // Access or create the 'users' database
   const usersDB = cloudant.use('users');
-
+  
   // Check if the email already exists
-  usersDB.get(email, (err, body) => {
+  usersDB.get(email, (err) => {
     if (!err) {
       // Email already exists
       res.status(400).send('Email already exists');
     } else {
       // Email does not exist, create new user
-      usersDB.insert({ _id: email, name: name, upassword: upassword }, (err, body) => {
+      usersDB.insert({ _id:email, name: name, upassword: upassword }, (err) => {
         if (err) {
           res.status(500).send('Error creating user');
         } else {
@@ -57,7 +53,7 @@ app.post('/signup', (req, res) => {
 
 app.post('/signin', (req, res) => {
   const { email, upassword } = req.body;
-
+  console.log(email,upassword);
   // Initialize Cloudant
   const cloudant = Cloudant({ url: url, username: username, password: password });
 
@@ -70,7 +66,7 @@ app.post('/signin', (req, res) => {
       res.status(400).send('Invalid email or password');
     } else {
       // Check if passwords match
-      if (body.password === password) {
+      if (body.upassword === upassword) {
         res.status(200).send('Sign in successful');
       } else {
         res.status(400).send('Invalid email or password');
